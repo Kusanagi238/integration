772f684664723ed892a646e54569a0b3e4e558d8
xcr <xcr@xcr>
2026-06-23 21:56:46 +0800

    Kusanagi238__diff__id_26

--- tests/repositories/test_get_documentation.py
+++ tests/repositories/test_get_documentation.py
@@ -1,4 +1,3 @@
-
 from typing import Any
 import pytest
 from custom_components.hacs.base import HacsBase
@@ -7,11 +6,13 @@ from custom_components.hacs.repositories.base import HacsRepository
 from tests.common import client_session_proxy
 
 
-
-@pytest.mark.parametrize("data,result", [
-    ({"installed": True, "installed_version": "1.0.0"}, "Example readme file"),
-    ({"installed": False, "last_version": "2.0.0"}, "Example readme file")
-])
+@pytest.mark.parametrize(
+    "data,result",
+    [
+        ({"installed": True, "installed_version": "1.0.0"}, "Example readme file"),
+        ({"installed": False, "last_version": "2.0.0"}, "Example readme file"),
+    ],
+)
 @pytest.mark.asyncio
 async def test_validate_repository(hacs: HacsBase, data: dict[str, Any], result: str):
     repository = HacsRepository(hacs=hacs)
